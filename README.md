f0b3d6aa7342ed5a55ba486247e66bd2a1b3f2f2
Fony <kamolphan.fony@gmail.com>
2020-10-24 00:08:51 +0700

    feat: add code to start / stop VM

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # simple-cloud-functions-vm-controller
-A Cloud Functions script to control VM, Google Compute Engine. Recommended to work with Cloud Scheduler.
+
+# Steps
+1. 
+2. Create a Pub/Sub topic. When deploy Cloud Functions via Console (web UI), there is a button to create a topic.
+3. Deploy Cloud Function. (alternatively: use `make deploy` as a prompt deploy command)
+4. Set Cloud Scheduler.
+
+# Deploy
+To use command line `make deploy`, Google Cloud SDK must be installed locally. (https://cloud.google.com/sdk/docs/install)
+
+The command will prompt for these information:
+```
+Enter function name: # The name of entrypoint function
+Enter project id   : # Project id, noted that it can be different from project name
+Enter env file     : # A yaml file that contains environment variables (optional)
+Enter cloud region : # A zone which the function will be deployed 
+Enter runtime      : # Runtime, for python, use `python37` or `python38`
+Enter topic name   : # A name of Cloud Pub/Sub topic for deploying pub/sub trigger function
+```
+
+# Reference
+- Google Cloud Functions example: https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/functions/helloworld/main.py
---
+++ main.py
@@ -0,0 +1,48 @@
+import base64
+import os
+import googleapiclient.discovery
+
+
+class Config:
+    name = os.environ.get("NAME")
+    project = os.environ.get("PROJECT")
+    zone = os.environ.get("ZONE")
+
+
+def start_vm(event, context):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+    try:
+        name = (
+            base64.b64decode(event["data"]).decode("utf-8")
+            if "data" in event
+            else Config.name
+        )
+    except ValueError:
+        name = Config.name
+    if Config.project is not None:
+        compute.instances().start(project=Config.project, zone=Config.zone, instance=name).execute()
+    else:
+        print("The .env.yaml is not found.")
+
+
+def stop_vm(event, context):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+    try:
+        name = (
+            base64.b64decode(event["data"]).decode("utf-8")
+            if "data" in event
+            else Config.name
+        )
+    except ValueError:
+        name = Config.name
+    if Config.project is not None:
+        compute.instances().stop(project=Config.project, zone=Config.zone, instance=name).execute()
+    else:
+        print("The .env.yaml is not found.")
+
+
+if __name__ == '__main__':
+    if Config.project is not None:
+        print("Test reading .env.yaml: project = ", Config.project)
+    else:
+        print("The .env.yaml is not found.")
